fb1bd1d41608c7ed70b4294f0aeeae610b6d1578
Seo_Hyun <04seohyun@gmail.com>
2020-08-04 11:53:53 +0900

    Init

---
+++ index.js
@@ -0,0 +1,52 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path')
+
+let win;
+
+ipcMain.on('restart', () => {
+    if (win) {
+        win.loadURL('');
+        setTimeout(() => {
+            win.loadURL(getFramePath());
+        }, 500);
+    }
+});
+
+function getFramePath(resName) {
+    let locale = app.getLocale();
+    let supportedLanguage = ['ko'];
+    if (!supportedLanguage.includes(locale)) locale = 'ko';
+    return `file://${__dirname}/frame.html?locale=${locale}&resName=${resName}.html`;
+}
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 900, height: 600, webPreferences: {
+            nodeIntegration: true,
+            webSecurity: false
+        }, icon: path.join(__dirname, 'logo.ico'), frame: false
+    });
+    win.setMenu(null);
+    win.loadURL(getFramePath());
+
+    //win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
